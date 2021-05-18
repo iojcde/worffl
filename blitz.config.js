2ba97d2b76263bfb801664c7c84011acb19bf20f
JcdeA <jcde@jcde.xyz>
2021-05-18 18:40:28 +0900

    chore: edit config files

--- blitz.config.js
+++ blitz.config.js
@@ -6,6 +6,9 @@ module.exports = {
       isAuthorized: simpleRolesIsAuthorized,
     }),
   ],
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
