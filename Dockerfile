d1640d0c00dd88cc025644bedb5b94bcb6b948ab
JcdeA <jcde@jcde.xyz>
2021-05-20 08:37:48 +0900

    fix: add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:latest
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package.json yarn.lock /usr/src/app/
+RUN YARN_CACHE_FOLDER=/dev/shm/yarn_cache yarn --production
+
+COPY .next /usr/src/app/.next
+COPY .blitz /usr/src/app/.blitz
+COPY public /usr/src/app/public
+
+EXPOSE 5000
+
+
+CMD [ "yarn", "start", "-p", "5000" ]
--- app/api/github/webhooks.ts
+++ app/api/github/webhooks.ts
@@ -1,13 +1,13 @@
-import { App, createNodeMiddleware } from "@octokit/app"
-import db from "db"
-import fs from "fs"
+import { App, createNodeMiddleware } from '@octokit/app'
+import db from 'db'
+import fs from 'fs'
 
 const clientID = process.env.GITHUB_CLIENT_ID as string,
   clientSecret = process.env.GITHUB_CLIENT_SECRET as string,
   webhookSecret = process.env.GITHUB_WEBHOOK_SECRET as string
 const app = new App({
   appId: 95733,
-  privateKey: fs.readFileSync("/home/jcde/sirius/dply-app.pem", "utf-8"),
+  privateKey: fs.readFileSync('/home/jcde/sirius/dply-app.pem', 'utf-8'),
   oauth: {
     clientId: clientID,
     clientSecret: clientSecret,
@@ -16,15 +16,15 @@ const app = new App({
     secret: webhookSecret,
   },
 })
-app.webhooks.on("installation.created", async ({ id, name, payload }) => {
-  console.log("received " + name)
+app.webhooks.on('installation.created', async ({ id, name, payload }) => {
+  console.log('received ' + name)
   const user = await db.user.findFirst({ where: { ghuserid: payload.sender.id } })
   console.log(user?.name)
   payload.repositories?.forEach(async (item) => {
     await db.ghRepo.upsert({
       where: { ghrepoid: item.id },
       create: {
-        url: "https://github.com/" + item.full_name,
+        url: 'https://github.com/' + item.full_name,
         ghrepoid: item.id,
         private: item.private,
         owner: {
@@ -34,5 +34,9 @@ app.webhooks.on("installation.created", async ({ id, name, payload }) => {
       update: { private: item.private },
     })
   })
+})
+
+app.webhooks.on('push', async ({ id, name, payload }) => {
+  // const project = await db.project.findFirst({ where: { GhRepo: { id: payload.repository.id } } })
 })
 export default createNodeMiddleware(app)
